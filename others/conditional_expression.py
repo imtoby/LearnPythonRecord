851e1cb8ca2336495b0e26d939b16d4c24383a07
imtoby <imtoby@126.com>
2018-02-26 09:33:48 +0800

    add conditional_expression.py

---
+++ others/conditional_expression.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+# Created by: ZhaoDongshuang
+# Created on: 18-2-12
+import math
+
+
+def test_source_01(x):
+    if x > 0:
+        y = math.log(x)
+    else:
+        y = float('nan')
+
+
+def test_source_01_conditional(x):
+    y = math.log(x) if x > 0 else float('nan')
+
+
+def factorial(n):
+    if n == 0:
+        return 1
+    else:
+        return n * factorial(n - 1)
+
+
+def factorial_conditional(n):
+    return 1 if n == 0 else n * factorial(n - 1)
+
+
+
